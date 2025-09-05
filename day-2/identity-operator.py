d31ed83e90eb74718600f41763d5efa5baf3af83
ishanrana363 <ishanrana363@gmail.com>
2025-09-05 21:42:25 +0600

    tody work completed

---
+++ day-2/identity-operator.py
@@ -0,0 +1,74 @@
+# Python Identity Operators
+# Identity operators are used to compare the objects,
+# not if they are equal, but if they are actually the same object, with the same memory location:
+
+
+
+# a = 2020
+# b = a
+#
+# c = a is b
+#
+# print(c)
+
+
+
+# a = 20
+# b = 20
+#
+# c = a is b
+#
+# print(c)
+#
+# print("a memory size",id(a))
+# print("b memory size",id(b))
+
+
+# a = 202020
+# b = 202020
+#
+# c = a is not b
+# print(c)
+# print("a memory size",id(a))
+# print("b memory size",id(b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day-2/membership-operator.py
@@ -0,0 +1,6 @@
+# Python Membership Operators
+# Membership operators are used to test if a sequence is presented in an object:
+
+marks = [10,33,23,45,98]
+b = 981 in marks
+print(b)
