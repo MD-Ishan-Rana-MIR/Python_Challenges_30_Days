4dc215c41748f03de73ffc968723c470e04e6f45
ishanrana363 <ishanrana363@gmail.com>
2025-09-05 19:51:14 +0600

    arithmetic operator completed

---
+++ day-2/arithmetic-operator.py
@@ -0,0 +1,86 @@
+# +	Addition	x + y
+# -	Subtraction	x - y
+# *	Multiplication	x * y
+# /	Division	x / y
+# %	Modulus	x % y
+# **	Exponentiation	x ** y
+# //	Floor division	x // y
+
+
+# addiction
+
+# x = 20
+# y = 30
+#
+# print(x+y)
+
+# Subtraction
+#
+# x = 30
+# y = 20
+#
+# z = x-y
+# print(z)
+
+# Multiplication
+
+# x = 30
+# y = 30
+#
+# z = x * y
+# print(z)
+
+# Division
+
+# x = 10
+# y = 20
+# z = x/y
+# print(z)
+
+# Modulus
+
+# a = 5
+# b = 2
+# c = a%b
+# print(c)
+
+# Exponentiation
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+# c = a ** b
+c = a//b
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
