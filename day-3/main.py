ff928b5e029bb698208a4b8a995a51d7dca36c37
ishanrana363 <ishanrana363@gmail.com>
2025-09-06 08:40:22 +0600

    if else elif completed

---
+++ day-3/main.py
@@ -0,0 +1,124 @@
+# grade_number = int(input("Enter your grade: "))
+#
+# if grade_number <= 25:
+#     print("Your grade is fail")
+# elif grade_number > 25 and grade_number <= 49:
+#     print("Your grade is E")
+# elif grade_number >=50 and grade_number <=59:
+#     print("Your grade is C")
+# else:
+#     print("Your grade is success")
+
+# num = int(input("Enter a number: "))
+#
+# if num % 2 == 0 and num % 5 == 0 :
+#     print(num, "is a perfect number")
+#
+# else :
+#     print(num, "is not a perfect number")
+
+
+amount = 0
+
+unit_number = float(input("Enter the amount of the unit: "))
+if unit_number<=100:
+    print("The amount of the unit is:", amount)
+elif unit_number>100 and unit_number <= 200 :
+    amount = unit_number * 5
+    print("The amount of the unit is:", amount)
+elif unit_number > 200 and unit_number < 300:
+    amount = unit_number * 10
+    print("The amount of the unit is:", amount)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
